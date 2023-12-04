147d5a6d69c18ee43ab6f352fb555780f471a271
Cyril DUPONT <cyril.dupont@ext.bpifrance.fr>
2023-12-04 20:44:13 +0100

    init typescript test project

--- java/range/src/test/java/RangeTest.java
+++ java/range/src/test/java/RangeTest.java
@@ -7,10 +7,10 @@ public class RangeTest {
     @Test
     void should_concat_two_string(){
         // GIVE
-        String helloWorld = "Hello World";
+        String helloWorld = "Hello";
 
         // WHEN
-        String res = helloWorld.concat("!");
+        String res = helloWorld.concat(" World!");
 
         // THEN
         assertThat(res).isEqualTo("Hello World!");
